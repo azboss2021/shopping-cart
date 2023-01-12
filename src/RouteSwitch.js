faa2cfe5e4f95d0eb8a9ce0382fa66a9965115cc
azboss2021 <alexanderzyn1@gmail.com>
2023-01-11 23:08:08 -0800

    Changed hashrouter location

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Pricing from './pages/Pricing';
@@ -14,38 +14,36 @@ export default function RouteSwitch() {
   const [cartCost, setCartCost] = useState(0);
 
   return (
-    <HashRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              products={products}
-              setProducts={setProducts}
-              productsLoaded={productsLoaded}
-              setProductsLoaded={setProductsLoaded}
-              cart={cart}
-              setCart={setCart}
-              setCartCost={setCartCost}
-            />
-          }
-        />
-        <Route
-          path="/cart"
-          element={
-            <Cart
-              cart={cart}
-              setCart={setCart}
-              cartCost={cartCost}
-              setCartCost={setCartCost}
-            />
-          }
-        />
-        <Route path="/pricing" element={<Pricing cart={cart} />} />
-        <Route path="/about" element={<About cart={cart} />} />
-        <Route path="/contact" element={<Contact cart={cart} />} />
-        <Route path="/checkout" element={<Checkout cart={cart} />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Home
+            products={products}
+            setProducts={setProducts}
+            productsLoaded={productsLoaded}
+            setProductsLoaded={setProductsLoaded}
+            cart={cart}
+            setCart={setCart}
+            setCartCost={setCartCost}
+          />
+        }
+      />
+      <Route
+        path="/cart"
+        element={
+          <Cart
+            cart={cart}
+            setCart={setCart}
+            cartCost={cartCost}
+            setCartCost={setCartCost}
+          />
+        }
+      />
+      <Route path="/pricing" element={<Pricing cart={cart} />} />
+      <Route path="/about" element={<About cart={cart} />} />
+      <Route path="/contact" element={<Contact cart={cart} />} />
+      <Route path="/checkout" element={<Checkout cart={cart} />} />
+    </Routes>
   );
 }
